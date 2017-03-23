1c692793b2b762480b38d427f10066220c5695c2
Admiral Potato <admiral@nuclearpixel.com>
2017-03-23 03:13:49 -0600

    replacing zipped jpeg output process with ffmpeg x265/HEVC(sadly yuv420)

--- video_portfolio_content.js
+++ video_portfolio_content.js
@@ -1,113 +1,23 @@
-//usage: node video_portfolio_content.js doubly_pyramidal-48-png 85
-//                              inputPath^                quality^
+//usage: node video_portfolio_content.js doubly_pyramidal-48-png
+//                              inputPath^
+
 var fs = require("fs");
-var JSZip = require("node-zip");
 var child_process = require("child_process");
-var jerpDerp = function(pngName){return pngName.replace(/png/gi, 'jpg');};
 var inputPath = process.argv[2];
+var inputName = inputPath.replace('-png', '');
 var quality = parseInt(process.argv[3], 10);
 var outputPrefix = 'output/';
-var outputPath = jerpDerp(outputPrefix + inputPath);
-var pathInt = function(path){
-	var file = path.split('/').pop();
-	var int = parseInt(file, 10);
-	return int;
-};
-var inputList = fs.readdirSync(inputPath);
-inputList = inputList
-	.filter(function(fileName){
-		return fileName.split('.').pop() === 'png';
-	})
-	.sort(function(a, b){
-		var A = pathInt(a);
-		var B = pathInt(b);
-		var result = 0;
-		if (A < B) {
-			result = -1;
-		}
-		if (A > B) {
-			result =  1;
-		}
-		return result;
-	});
 if(!fs.existsSync(outputPrefix)){
 	fs.mkdirSync(outputPrefix);
 }
-if(!fs.existsSync(outputPath)){
-	fs.mkdirSync(outputPath);
-}
 
-var outputPathList = [];
-
-var zipImagesWhenDone = function(){
-	var zip = new JSZip();
-	var zipName = outputPath + '-' + quality + '.zip';
-	outputPathList.forEach(function(readImagePath){
-		var fileData = fs.readFileSync(readImagePath);
-		var fileName = readImagePath.split('/').pop();
-		zip.file(
-			fileName,
-			fileData
-		);
-	});
-	var data = zip.generate({
-		base64:false,
-		compression:'DEFLATE',
-		compressionOptions: {
-			level: 9
-		}
-	});
-	fs.writeFileSync(zipName, data, 'binary');
-	console.log('Finished creating zip: ' + zipName);
-};
-
-console.time(outputPath);
-var completed = 0;
-inputList.forEach(
-	function(imageName){
-		var inputPathString = inputPath + '/' + imageName;
-		var outputPathString = outputPrefix + jerpDerp(inputPathString);
-		var start = Date.now();
-		outputPathList.push(outputPathString);
-		child_process.exec(
-			[
-				'node',
-				'process_one_image.js',
-				inputPathString,
-				outputPathString,
-				quality
-			].join(' '),
-			function(err, stdout, stderr){
-				if (err) {
-					console.error(err);
-					return;
-				}
-				var time = (Date.now() - start);
-				console.log([stdout, time, 'ms'].join(' ').replace(/\n/g, ' '));
-
-				completed++;
-				if(completed === inputList.length){
-					console.timeEnd(outputPath);
-					zipImagesWhenDone()
-				}
-			}
-		);
-	}
-);
-
-var makeThumb = function(){
-	var inputPathString = inputPath + '/0001.png';
-	var outputPathString = outputPath.replace('-jpg', '.jpg');
+var childProcessIt = function(command, logName){
 	var start = Date.now();
+	console.log(logName + ' - Start');
 	child_process.exec(
-		[
-			'node',
-			'make_thumb.js',
-			inputPathString,
-			outputPathString,
-			quality
-		].join(' '),
+		command,
 		function(err, stdout, stderr) {
+			console.log(logName + ' - End');
 			if (err) {
 				console.error(err);
 				return;
@@ -117,4 +27,56 @@ var makeThumb = function(){
 		}
 	);
 };
+
+var formatList = [
+	{
+		resolution: [1920,1080],
+		bitrate: 20000
+	},
+	{
+		resolution: [960,540],
+		bitrate: 5000
+	}
+];
+
+var makeEncodeCommand = function(format) {
+	var pixFormat = 'yuv420p'; //change to 444 later when the client side can handle it
+	var resolution = format.resolution.join('x');
+	var bitrate = format.bitrate;
+	var outputPathString = outputPrefix + [inputName, resolution, pixFormat, bitrate].join('-') + '.hevc';
+	return [
+		'ffmpeg -framerate 24 -f image2 -i',
+		inputPath + '/%04d.png',
+		'-vcodec rawvideo -keyint_min 1',
+		'-s ' + resolution,
+		'-r 24',
+		'-pix_fmt ' + pixFormat,
+		'-c:v libx265',
+		'-b:v ' + bitrate + 'k',
+		'-preset veryslow -x265-params keyint=1:ref=1:no-open-gop=1:weightp=0:weightb=0:cutree=0:rc-lookahead=0:bframes=0:scenecut=0:b-adapt=0:repeat-headers=1',
+		outputPathString
+	].join(' ');
+};
+
+var makeVideos = function () {
+	formatList.forEach(function(format){
+		var command = makeEncodeCommand(format);
+		childProcessIt(command, 'makeVideo' + format.resolution.join('x'));
+	});
+};
+
+var makeThumb = function(){
+	var inputPathString = inputPath + '/0001.png';
+	var outputPathString = outputPrefix + inputName + '.jpg';
+	var command = [
+			'node',
+			'make_thumb.js',
+			inputPathString,
+			outputPathString,
+			quality
+		].join(' ');
+	childProcessIt(command, 'makeThumb');
+};
+
+makeVideos();
 makeThumb();
